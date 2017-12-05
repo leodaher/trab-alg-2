442099bfd8284723bcc2e18b201b3a4b76080084
gbnogima <gbnogima@gmail.com>
2017-12-04 22:09:43 -0200

    boa noite a todos

---
+++ t2.h
@@ -0,0 +1,23 @@
+#ifndef TRABALHO3_H_INCLUDED
+#define TRABALHO3_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BTREE_FILE	"arvore.idx"
+#define DATA_FILE	"dados.dad"
+
+/* Tamanho do maior inteiro que cabe em um char + '\0' */
+#define REGFILE_BUFFERSIZE 256
+
+/* Marcador de registro removido */
+#define REGFILE_REGREMOVED "*|"
+
+typedef struct {
+	int id;
+	char titulo[30];
+	char genero[20];
+} registro_t;
+
+#endif
